4fdd836263cd502bd68255ffe4420d1eadeb9b3f
Carsten Kremser <github@carstenkremser.de>
2024-04-08 12:04:52 +0200

    Add UnitTest

---
+++ src/test/java/de/carstenkremser/neuefische/asterix/service/CharacterServiceTest.java
@@ -0,0 +1,24 @@
+package de.carstenkremser.neuefische.asterix.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void updateName() {
+    }
+
+    @Test
+    void updateProfession() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
